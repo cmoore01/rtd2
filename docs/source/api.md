eef0ab6d21404070784e7a8d82c40c4ffd7c3a9f
Charlie Moore <moore.c@wustl.edu>
2024-10-08 14:52:15 -0500

    Testing

---
+++ docs/source/api.md
@@ -0,0 +1,11 @@
+# API
+
+```{eval-rst}
+.. autosummary::
+   :toctree: generated
+
+   lumache
+```
+
+.. openapi:: specs/openapi.yml
+
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,12 +11,16 @@ version = '0.1.0'
 
 # -- General configuration
 
+source_suffix = ['.rst', '.md']
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinxcontrib.openapi',
+    'myst_parser'
 ]
 
 intersphinx_mapping = {
