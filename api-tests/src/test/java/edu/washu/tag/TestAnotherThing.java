649eefa22ec2dd68fdd9f04311be5873619303af
Charlie Moore <moore.c@wustl.edu>
2024-10-11 16:06:51 -0500

    Parallel test experimentation

--- api-tests/src/test/java/edu/washu/tag/TestAnotherThing.java
+++ api-tests/src/test/java/edu/washu/tag/TestAnotherThing.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.assertEquals;
 
-public class TestExample extends BaseTestCase {
+public class TestAnotherThing extends BaseTestCase {
 
     @Test
     public void testThing() {
